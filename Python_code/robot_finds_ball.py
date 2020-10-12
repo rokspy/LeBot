8ed87a8cee14a4c302ec6a5299d70aaf827a448e
nap <45245133+napdot@users.noreply.github.com>
2020-10-12 16:24:57 +0300

    Update robot_finds_ball.py

--- Python_code/robot_finds_ball.py
+++ Python_code/robot_finds_ball.py
@@ -1,4 +1,4 @@
-from Vision import ImageProcessing
+from ReworkVision import ImageProcessing
 from TempMovement import MainBoardMovement
 
 See = ImageProcessing
@@ -7,19 +7,26 @@ x_mid = 1280
 x_range = 10
 rot_quickness = 5
 
-
 print('Starting orientation...')
 
 while True:
-    BallCentroid = See.return_basket_centroid # Centroid
-    if x_mid - x_range > BallCentroid[0] > x_mid + x_range: # If not withing horizontal range
+    BallCentroid = See.return_basket_centroid  # Centroid
+    if x_mid - x_range > BallCentroid[0] > x_mid + x_range:  # If not withing horizontal range
         dif = BallCentroid - x_mid
-        if dif > 0:     # Ball is to the right
+        if dif > 0:  # Ball is to the right
             Move.rotate_right(rot_quickness)
-        elif dif < 0:   # Ball is to the left
+        elif dif < 0:  # Ball is to the left
             Move.rotate_left(rot_quickness)
         continue
+
+    elif BallCentroid == None:
+        Move.rotate_right(rot_quickness)
+        continue
+
     else:
         break
 
 print("Orientation done")
+
+Move.ser_stop()
+See.stop_all()
